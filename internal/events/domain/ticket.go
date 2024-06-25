0f4016f31405e1b578389efb4a0ab0065b27cfe3
ismaelp96 <ismael6991@live.com>
2024-06-25 09:46:13 -0300

    ticket.go and repository,go

--- internal/events/domain/ticket.go
+++ internal/events/domain/ticket.go
@@ -1,6 +1,12 @@
 package domain
 
+import "errors"
+
 type TicketType string
+
+var (
+	ErrTicketPriceZero = errors.New("Ticket price must be greater than zero")
+)
 
 const (
 	TicketTypeHalf TicketType = "half" // Half-price ticket
@@ -8,9 +14,29 @@ const (
 )
 
 type Ticket struct {
-	ID       string
-	EventID  string
-	Spot     *Spot
-	TicketID TicketType
-	Price    float64
+	ID         string
+	EventID    string
+	Spot       *Spot
+	TicketType TicketType
+	Price      float64
+}
+
+func isValidTicketType(ticketType TicketType) bool {
+	return ticketType == TicketTypeHalf || ticketType == TicketTypeFull
+}
+
+func (t *Ticket) CalculatePrice() {
+	if t.TicketType == TicketTypeHalf {
+		t.Price /= 2
+	}
+	// } else {
+	// 	t.Price = t.Spot.Event.Price
+	// }
+}
+
+func (t *Ticket) Validate() error {
+	if t.Price <= 0 {
+		return ErrTicketPriceZero
+	}
+	return nil
 }
